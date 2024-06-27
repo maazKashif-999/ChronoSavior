97f4be1c7a8f9de6686518de44d217ea95af0db3
Muhammad Saram Hassan <134005322+msaramhassan@users.noreply.github.com>
2024-06-28 00:10:27 +0500

    space audio effects

--- Chrono Savior/Assets/Scripts/Space/Player/PlayerControls.cs
+++ Chrono Savior/Assets/Scripts/Space/Player/PlayerControls.cs
@@ -25,9 +25,17 @@ public class PlayerControls : MonoBehaviour
     private int coins = 0; // Variable to keep track of collected coins
     private int token = 0;
 
+    public AudioClip bulletSound; //variable to store bullet sound
+    public AudioClip explodeSound;
+    private AudioSource audioSource;
+
+
+
     private void Start()
     {
         gameManager = FindObjectOfType<SpaceGameManager>(); // Find the GameManager in the scene
+        audioSource = GetComponent<AudioSource>();
+
     }
 
     public void Init()
@@ -71,6 +79,7 @@ public class PlayerControls : MonoBehaviour
         // Fire bullet when user clicks left mouse button or touches screen
         if (Input.GetMouseButtonDown(0))
         {
+
             FireBullet();
         }
         Move();
@@ -92,12 +101,18 @@ public class PlayerControls : MonoBehaviour
             Camera mainCamera = Camera.main;
             if (mainCamera != null)
             {
+                
                 Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
                 mousePosition.z = 0f;
                 PlayerBullet bulletScript = bulletObject.GetComponent<PlayerBullet>();
                 if (bulletScript != null)
                 {
                     bulletScript.Initialize(mousePosition, bulletSpeed, damage, Quaternion.Euler(0, 0, 0));
+                }
+
+                if (bulletSound != null && audioSource != null)
+                {
+                    audioSource.PlayOneShot(bulletSound);
                 }
             }
             else
@@ -175,6 +190,12 @@ public class PlayerControls : MonoBehaviour
         {
             GameObject explosionObject = Instantiate(explosion, transform.position, Quaternion.identity);
         }
+
+        if (explodeSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(explodeSound);
+        }
+
         else
         {
             Debug.LogWarning("Explosion prefab is not assigned.");

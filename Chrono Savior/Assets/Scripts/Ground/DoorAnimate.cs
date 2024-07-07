3e397b008aa876e6bc8df6aff62796772aa2c8f6
Muhammad Saram Hassan <134005322+msaramhassan@users.noreply.github.com>
2024-07-07 17:51:47 +0500

    hahaha

--- Chrono Savior/Assets/Scripts/Ground/DoorAnimate.cs
+++ Chrono Savior/Assets/Scripts/Ground/DoorAnimate.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class DoorAnimate : MonoBehaviour
 {
+    private AudioSource myaudio;
+    [SerializeField] private AudioClip doorOpenSound;
+    [SerializeField] private AudioClip doorCloseSound;
+
     private Animator animator;
     private const string IS_OPEN = "IsOpen";
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        myaudio = GetComponent<AudioSource>();
     }
     public void OpenDoor()
     {
@@ -19,6 +24,16 @@ public class DoorAnimate : MonoBehaviour
         else
         {
             Debug.LogError("Animator is null");
+        }
+
+        if(myaudio!=null && doorOpenSound!=null)
+        {
+            myaudio.PlayOneShot(doorOpenSound);
+        }
+
+        else
+        {
+            Debug.Log("door opening sound not found");
         }
     }
 
@@ -31,6 +46,16 @@ public class DoorAnimate : MonoBehaviour
         else
         {
             Debug.LogError("Animator is null");
+        }
+
+        if (myaudio != null && doorCloseSound != null)
+        {
+            myaudio.PlayOneShot(doorCloseSound);
+        }
+
+        else
+        {
+            Debug.Log("door closing sound not found");
         }
     }
 }
